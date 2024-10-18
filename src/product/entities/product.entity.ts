c50b0b9c1b5a622731f656eb738f2508f0fcaf63
Abdulhalık İşcan <94014015+iscanabdulhalik@users.noreply.github.com>
2024-10-18 13:44:16 -0700

    #1: Create Product Model and Product Table

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,15 @@
-export class Product {}
+import { PrimaryGeneratedColumn, Column } from 'typeorm';
+
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  stock: number;
+}
