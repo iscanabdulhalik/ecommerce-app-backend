bd939bebb7c6ea60941387d9982310714c5cd5b6
Abdulhalık İşcan <94014015+iscanabdulhalik@users.noreply.github.com>
2024-10-18 23:34:13 +0300

    Revert "#1: Refactor product entity to include id, name, price, and stock fields"
    This reverts commit 7dc9a6903f0f9474807510c0f5e7ab95d41e84d4.

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,15 +1,1 @@
-import { PrimaryGeneratedColumn, Column } from 'typeorm';
-
-export class Product {
-  @PrimaryGeneratedColumn()
-  id: string;
-
-  @Column()
-  name: string;
-
-  @Column()
-  price: number;
-
-  @Column()
-  stock: number;
-}
+export class Product {}
