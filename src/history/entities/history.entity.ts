0080026e7bbc1383562bc7e0ef78e895889e6214
Abdulhalık İşcan <94014015+iscanabdulhalik@users.noreply.github.com>
2024-10-18 13:46:59 -0700

    #2: Create History Model and History Table

--- src/history/entities/history.entity.ts
+++ src/history/entities/history.entity.ts
@@ -1,1 +1,19 @@
-export class History {}
+import { PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { User } from 'src/user/entities/user.entity';
+
+export class History {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  date: Date;
+
+  @Column()
+  from: string;
+
+  @Column({ type: 'jsonb' })
+  details: Record<string, any>; // JSON formatinda veri saklamak icin
+
+  @ManyToOne(() => User, (user) => user.history, { onDelete: 'CASCADE' })
+  user: User;
+}
